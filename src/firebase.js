d7b0a475ab43a7a0ef5852574f3320dfbd421205
tokyopistol <tokyopistol1975@gmail.com>
2021-01-08 00:23:01 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA6wzQ1xr8uQcow2SHhjYqtHANrCJzOOc",
+  authDomain: "idobatakaigi-with-ta.firebaseapp.com",
+  projectId: "idobatakaigi-with-ta",
+  storageBucket: "idobatakaigi-with-ta.appspot.com",
+  messagingSenderId: "947083580229",
+  appId: "1:947083580229:web:150d1bcfd9e5727dace42c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
